6f5ba35fd94045c6fd05147eec7965c4d13b6104
Vasylyna <v.i.mykhalchuk@gmail.com>
2017-03-05 21:02:10 +0200

    Create converters

---
+++ StudentsManager/StudentsManager/Converters/BoolToStringConverter.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace StudentsManager.Converters
+{
+    [ValueConversion(typeof(object), typeof(string))]
+    public class BoolToStringConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var boolValue = Boolean.Parse(value.ToString());
+
+            if (boolValue)
+                return "yes";
+            return "no";
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ StudentsManager/StudentsManager/Converters/ListViewWidthConverter.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace StudentsManager.Converters
+{
+    public class ListViewWidthConverter : IValueConverter
+    {
+       public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+       {
+           const double coeff = 200.0;
+           var windowWidth = (double) value;
+           return windowWidth - coeff;
+       }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
